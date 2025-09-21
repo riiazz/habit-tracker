ac1f2d5fb10b0597dc8f463d3acee835af1709ae
Rizaldi Aziz <rizaldiaziz18@gmail.com>
2025-09-21 23:16:55 +0700

    setup google sheet integration

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf, sync::Arc};
 
 use chrono::{Duration, Utc};
-use yup_oauth2::{ServiceAccountAuthenticator, read_service_account_key};
+use google_sheets4::Sheets;
+use serde::Deserialize;
+use yup_oauth2::{ServiceAccountAuthenticator, hyper, hyper_rustls, read_service_account_key};
 
 #[tokio::main]
 async fn main() {
@@ -10,6 +12,18 @@ async fn main() {
     let date_format = "%Y-%m-%d %H:%M";
 
     println!("{}", wib.format(date_format));
+
+    let config_path = dirs::config_dir()
+        .unwrap()
+        .join("habit_tracker/config.toml");
+
+    let content = fs::read_to_string(config_path).expect("Failed to read config file");
+    let app_config: AppConfig = toml::from_str(&content).expect("Failed to parse config.toml");
+
+    println!(
+        "Spreadsheet id: {}, sheet name: {}",
+        app_config.spreadsheet_id, app_config.sheet_name
+    );
 
     let creds_path: PathBuf = dirs::config_dir()
         .unwrap()
@@ -24,6 +38,45 @@ async fn main() {
         .await
         .unwrap();
 
+    //let auth = Arc::new(auth);
+
     println!("Successfully load credentials!");
     println!("Credential path: {}", creds_path.display());
+
+    let https = hyper_rustls::HttpsConnectorBuilder::new()
+        .with_native_roots()
+        .unwrap()
+        .https_or_http()
+        .enable_http1()
+        .build();
+
+    let hub = Sheets::new(hyper::Client::builder().build(https), auth);
+    let range = format!("{}!A1:AF9", app_config.sheet_name);
+
+    let result = hub
+        .spreadsheets()
+        .values_get(&app_config.spreadsheet_id, &range)
+        .doit()
+        .await;
+
+    match result {
+        Ok((_resp, value_range)) => {
+            if let Some(values) = value_range.values {
+                for row in values {
+                    println!("{:?}", row);
+                }
+            } else {
+                println!("No data in range {}", range);
+            }
+        }
+        Err(e) => {
+            eprintln!("API error: {:?}", e);
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct AppConfig {
+    spreadsheet_id: String,
+    sheet_name: String,
 }
